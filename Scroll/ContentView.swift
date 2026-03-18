963db0cf4a1ec0e3ca53024f4594b69e592a6fc2
Adon Omeri <adon.omeri@student.education.wa.edu.au>
2026-03-18 14:03:46 +0800

    Initial Commit

---
+++ Scroll/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Scroll
+//
+//  Created by Adon Omeri on 18/3/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Scroll/ScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollApp.swift
+//  Scroll
+//
+//  Created by Adon Omeri on 18/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct ScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
