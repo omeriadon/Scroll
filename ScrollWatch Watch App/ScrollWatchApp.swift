3a408912570ce52234e22f699f91729161eaef71
Adon Omeri <adon.omeri@student.education.wa.edu.au>
2026-03-18 14:14:55 +0800

    init

---
+++ ScrollWatch Watch App/ScrollWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollWatchApp.swift
+//  ScrollWatch Watch App
+//
+//  Created by Adon Omeri on 18/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct ScrollWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
